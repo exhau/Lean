276576cc29559b14e5a65abbbf89a693ca7817f9
chunw <exhau@msn.com>
2015-07-19 02:07:56 -0400

    Added a machine learning test algo.

---
+++ Algorithm.CSharp/Amigo/MachineLearningTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using QuantConnect.Algorithm;
+using QuantConnect.Data.Custom;
+using QuantConnect.Data.Market;
+using QuantConnect.Orders;
+
+namespace QuantConnect.Algorithm.CSharp.Amigo
+{
+    public class MachineLearningTest : QCAlgorithm
+    {
+    }
+}
